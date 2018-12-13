72e8c5b4401917e595c6cf6dde4e3f6ccdfd989f
marciorpcoelho <marciorpcoelho@gmail.com>
2018-12-13 14:32:21 +0000

    Section B: Implemented function to analyze the present of infinities (postive and negative);

--- level_1_b_data_processing.py
+++ level_1_b_data_processing.py
@@ -94,6 +94,18 @@ def null_analysis(df):
     print(tab_info)
 
 
+def inf_analysis(df):
+    # Displays the number and percentage of null values in the DF
+
+    tab_info = pd.DataFrame(df.dtypes).T.rename(index={0: 'column type'})
+    tab_info = tab_info.append(pd.DataFrame((df == np.inf).astype(int).sum(axis=0)).T.rename(index={0: '#null:'}))
+    tab_info = tab_info.append(pd.DataFrame((df == -np.inf).astype(int).sum(axis=0)).T.rename(index={0: '#null:'}))
+    tab_info = tab_info.append(pd.DataFrame((df == np.inf).astype(int).sum() / df.shape[0] * 100).T.rename(index={0: '%null:'}))
+    tab_info = tab_info.append(pd.DataFrame((df == -np.inf).astype(int).sum() / df.shape[0] * 100).T.rename(index={0: '%null:'}))
+
+    print(tab_info)
+
+
 def zero_analysis(df):
     # Displays the number and percentage of null values in the DF
 
