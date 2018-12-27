170f40279c3db4535e52c1f8b50f019b2795c81a
marciorpcoelho <marciorpcoelho@gmail.com>
2018-12-27 16:51:01 +0000

    Section B: Updated the warning message for values not grouped;

--- level_1_b_data_processing.py
+++ level_1_b_data_processing.py
@@ -490,7 +490,10 @@ def col_group(df, columns_to_replace, dictionaries):
                 df.loc[df[column].isin(dictionary[key]), column + '_new'] = key
             if df[column + '_new'].isnull().values.any():
                 variable = df.loc[df[column + '_new'].isnull(), column].unique()
-                print('Column Grouping Warning - NaNs detected in: {}'.format(column + '_new, value(s) not grouped: {}'.format(variable)))
+                # print('Column Grouping Warning - NaNs detected in: {}'.format(column + '_new, value(s) not grouped: {}'.format(variable)))
+                level_2_optionals_baviera_performance_report_info.performance_warnings_append('Column Grouping Warning - NaNs detected in: {}'.format(columns_to_replace[dictionaries.index(dictionary)] + '_new, value(s) not grouped: {}'.format(variable)))
+                level_2_optionals_baviera_performance_report_info.performance_warnings_append('Column Grouping Warning - NaNs detected from vehicle with VHE_Number' + str(df[df[column + '_new'].isnull()]['Nº Stock']))
+                level_2_optionals_baviera_performance_report_info.log_record('Column Grouping Warning - NaNs detected in: {}'.format(columns_to_replace[dictionaries.index(dictionary)] + '_new, value(s) not grouped: {}'.format(variable)), sql_info['database'], sql_info['log_record'], flag=1)
             df.drop(column, axis=1, inplace=True)
             df.rename(index=str, columns={column + '_new': column}, inplace=True)
         except KeyError:
