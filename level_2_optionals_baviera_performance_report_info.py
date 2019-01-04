65218a0ffa66a89926c824657a2e43705e60a27f
marciorpcoelho <marciorpcoelho@gmail.com>
2019-01-04 17:52:48 +0000

    Level 2 Options: updated update_frequency_days to 0 to make sure the script fetches the updated data; Level 2 Performance Report: Changed the emails back from email_notification to the ones retrieved from SQL;

--- level_2_optionals_baviera_performance_report_info.py
+++ level_2_optionals_baviera_performance_report_info.py
@@ -57,10 +57,8 @@ def email_notification(warning_flag, warning_desc, error_desc, error_flag=0, mod
     run_conclusion, warning_conclusion, conclusion_message = None, None, None
     fromaddr = 'mrpc@gruposalvadorcaetano.pt'
     df_mail_users = level_1_a_data_acquisition.sql_retrieve_df(level_2_optionals_baviera_options.sql_info['database'], level_2_optionals_baviera_options.sql_info['mail_users'])
-    # users = df_mail_users['UserName'].unique()
-    users = ['Marcio']
-    # toaddrs = df_mail_users['UserEmail'].unique()
-    toaddrs = ['marcio.coelho@rigorcg.pt']
+    users = df_mail_users['UserName'].unique()
+    toaddrs = df_mail_users['UserEmail'].unique()
 
     mail_subject = 'Otimização Encomenda - Relatório'
 
